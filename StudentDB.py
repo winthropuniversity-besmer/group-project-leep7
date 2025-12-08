d52eea8a452df8b6f93f13d2fed7006f3ad2c204
leep7 <143541897+leep7@users.noreply.github.com>
2025-12-08 14:44:57 -0500

    Add files via upload

---
+++ StudentDB.py
@@ -0,0 +1,89 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="deltona.birdnest.org",
+    user="my.leep7",
+    password="",
+    database="sample"
+)
+
+mycursor = mydb.cursor()
+sql_create = "INSERT INTO students (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
+sql_create = "INSERT INTO courses (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
+sql_create = "INSERT INTO grades (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
+sql_edit = "UPDATE students SET name = %s, age = %s WHERE id = %s"
+sql_edit = "UPDATE grades SET name = %s, age = %s WHERE id = %s"
+sql_delete = "DELETE FROM students where id = %s"
+
+
+class StudentDB:
+def add students(self,first_name, last_name, email,):
+def add courses(self, course_id, course_name):
+def add grades(self, grade_id, student_id, course_id, grade);
+def update student():
+def update grades():
+def delete student():
+
+   
+def menu(): #shows the choices 
+    print("1. Add Student")
+    print("2. Add Course")
+    print("3. Add Grade")
+    print("4. Update Student")
+    print("5. Update Grade")
+    print("6. Delete Student")
+    print("7. Exit")
+    return input("Choice: ")
+
+
+def main(): #
+
+    while True:  #keeps showing the menu until user chooses 
+        choice = menu()
+
+        if choice == '1':
+            db.add_student(
+                input("First Name: "),
+                input("Last Name: "),
+                input("Email: "),
+                input("Phone: ")
+            )
+
+        if choice == '2':
+            db.add_course(
+                input("Course ID: "), 
+                input("Course Name")
+
+            )
+
+        if choice == '3':
+            db.add_grade(
+                int(input("Student ID: ")),
+                int(input("Course ID: ")),
+                input("Grade: ")
+            )
+
+        if choice == '4':
+            db.update_student(
+                int(input("Student ID: ")),
+                input("New Email: "),
+                input("New Phone: ")
+            )
+
+        if choice == '5':
+            db.update_grade(
+                int(input("Grade ID: ")),
+                input("New Grade: ")
+            )
+
+        if choice == '6':
+            db.delete_student(int(input("Student ID: ")))
+
+        if choice == '7':
+            print("Bye!")
+            break
+
+        else:
+            print("Invalid choice!")
+
+
