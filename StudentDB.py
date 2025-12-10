744336101852e39de36e4f4ac53db6dd06e5c13e
COwinthrop <overbyc2@winthrop.edu>
2025-12-09 22:57:12 -0500

    Added functionality for searching students, grades, and courses. Replaced if sturcuture with match sturcutre for simplicity.

--- StudentDB.py
+++ StudentDB.py
@@ -8,11 +8,16 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor() #these can be inserted into the class StudentDB
-sql_create = "INSERT INTO students (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
-sql_create = "INSERT INTO courses (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
-sql_create = "INSERT INTO grades (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
-sql_edit = "UPDATE students SET name = %s, age = %s WHERE id = %s"
-sql_edit = "UPDATE grades SET name = %s, age = %s WHERE id = %s"
+
+sql_createStudents = "INSERT INTO Students (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
+sql_createCourses = "INSERT INTO Courses (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
+sql_createGrades = "INSERT INTO Grades (first_name, last_name, email, phone_number) VALUES (%s, %s, %s, %s)"
+sql_editStudents = "UPDATEGtudents SET name = %s, age = %s WHERE id = %s"
+sql_editGrades = "UPDATE Grades SET name = %s, age = %s WHERE id = %s"
+sql_editCourses = "UPDATE Courses SET name = %s, age = %s WHERE id = %s"
+sql_selectStudent = "SELECT * FROM Students" #Commands for cursor to execute to select from the 3 tables
+sql_selectCourse = "SELECT * FROM Courses"
+sql_selectGrade = "SELECT * FROM Grades"
 sql_delete = "DELETE FROM students where id = %s"
 
 
@@ -33,65 +38,85 @@ def menu(): #shows the choices
     print("3. Add Grade")
     print("4. Update Student")
     print("5. Update Grade")
-    print("6. Select Student")
-    print("7. Delete Student")
-    print("8. Exit")
+    print("6. Search Student")
+    print("7. Search Course")
+    print("8. Search Grade")
+    print("9. Delete Grade")
+    print("10. Exit")
     return input("Choice: ")
 
 
-def main(): #
+def main():
+    choice = 0
+    while choice != 10:
+        os.system('cls')
+        choice = int(menu())
 
-    while True:  #keeps showing the menu until user chooses 
-        choice = menu()
-
-        if choice == '1':
-            db.add_student(
+        match choice:    
+            case 1:
+                db.add_student(
                 input("First Name: "),
                 input("Last Name: "),
                 input("Email: "),
                 input("Phone: ")
-            )
+                )
+            case 2:
+                db.add_student(
+                input("First Name: "),
+                input("Last Name: "),
+                input("Email: "),
+                input("Phone: ")
+                )
 
-        if choice == '2':
-            db.add_course(
-                input("Course ID: "), 
-                input("Course Name")
-
-            )
-
-        if choice == '3':
-            db.add_grade(
+            case 3:
+                db.add_grade(
                 int(input("Student ID: ")),
                 int(input("Course ID: ")),
                 input("Grade: ")
-            )
-
-        if choice == '4':
-            db.update_student(
+                )
+            case 4:
+                 db.update_student(
                 int(input("Student ID: ")),
                 input("New Email: "),
                 input("New Phone: ")
-            )
-
-        if choice == '5':
-            db.update_grade(
+                )
+            case 5:
+                db.update_grade(
                 int(input("Grade ID: ")),
                 input("New Grade: ")
-            )
+                )
+            case 6:
+                student = int(input("Provide Student ID: "))
+                mycursor.execute(sql_selectStudent, (student,))
+                output = mycursor.fetchall()
+                for row in output:
+                    print(row)
+                input("Press Enter to continue...")
+            case 7:
+                course = int(input("Provide Course ID: "))
+                mycursor.execute(sql_selectCourse, (course,))
+                output = mycursor.fetchall()
+                for row in output:
+                    print(row)
+                input("Press Enter to continue...")
+            case 8:
+                grade = int(input("Provide Grade ID: "))
+                mycursor.execute(sql_selectGrade, (grade,))
+                output = mycursor.fetchall()
+                for row in output:
+                    print(row)
+                input("Press Enter to continue...")
+            case 9:
+                db.delete_student(int(input("Student ID: ")))
+            case 10:
+                print("Bye!")
+                mycursor.close()
+                mydb.close()
+                break
+            case _:
+                print("Invalid choice!")
 
-         if choice == '6':
-            db.show_students()
-
-        if choice == '7':
-            db.delete_student(int(input("Student ID: ")))
-
-        if choice == '8':
-            print("Bye!")
-            break
-
-        else:
-            print("Invalid choice!")
-
+main()
 
 
 
