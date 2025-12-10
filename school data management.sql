a8f31c762b4a92bc3a2f482238d7cb3bc7b727e3
leep7 <143541897+leep7@users.noreply.github.com>
2025-12-10 14:49:49 -0500

    Update school data management.sql

--- school data management.sql
+++ school data management.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE Students (
+CREATE TABLE students (
     student_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE Students (
     PRIMARY KEY (student_id)
 );
 
-INSERT INTO Students (student_id, first_name, last_name, email, phone_number)
+INSERT INTO students (student_id, first_name, last_name, email, phone_number)
 VALUES 
     (1, 'Jonathan', 'Gilbert', 'jonathan.gilbert@winthrop.edu', '7575746746'),
     (2, 'Jane', 'Smith', 'jane.smith@winthrop.edu', '64758574793'),
@@ -16,13 +16,13 @@ VALUES
     (4, 'Ronald', 'McDonald', 'ronald.mcdonald@winthrop.edu', '3746378498'),
     (5, 'Mike', 'Johnson', 'mike.johnson@winthrop.edu', '3647584637');
 
-CREATE TABLE Courses (
+CREATE TABLE courses (
     course_id INT NOT NULL AUTO_INCREMENT,
     course_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (course_id)
 );
 
-INSERT INTO Courses (course_id, course_name)
+INSERT INTO courses (course_id, course_name)
 VALUES
     (101,'Introduction to Mathematics'),
     (102,'Introduction to Science'),
@@ -30,7 +30,7 @@ VALUES
     (104, 'Database Processing'),
     (105, 'Biology');
 
-CREATE TABLE Grades (
+CREATE TABLE grades (
     grade_id INT NOT NULL AUTO_INCREMENT,
     student_id INT NOT NULL,
     course_id INT NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE Grades (
         ON UPDATE CASCADE 
 );
 
-INSERT INTO Grades (student_id, course_id, grade) 
+INSERT INTO grades (student_id, course_id, grade) 
 VALUES
     (1, 102, 'A'),
     (2, 101, 'C'),
@@ -53,6 +53,7 @@ VALUES
     (3, 104, 'A'),
     (4, 105, 'B'),
     (5, 101, 'C');
+
 
 
 
